c67b6c72011fcb0978a83f93803ac85d7bae4177
oussamaaouam1 <oussama.aouam@gmail.com>
2024-11-15 22:15:57 +0100

    upload screens for my projects

--- src/components/maincontent/MainContent.jsx
+++ src/components/maincontent/MainContent.jsx
@@ -1,17 +1,55 @@
 import { useState } from "react";
 import "./maincontent.css";
 
-
-
-
 const myprojects = [
-  { projectTitle: "react project", category: "react.js", imgPath: "a" },
-  { projectTitle: "html&css project", category: "htmlcss", imgPath: "b" },
+  {
+    projectTitle: "Currency Converter",
+    category: "react",
+    imgPath: "public/currency converter react.png",
+  },
+  {
+    projectTitle: "Headphones",
+    category: "htmlcss",
+    imgPath: "public/headphones HTML&CSS.png",
+  },
+  {
+    projectTitle: "Landing Page",
+    category: "react",
+    imgPath: "public/landing page react.png",
+  },
+  {
+    projectTitle: "html&css project",
+    category: "htmlcss",
+    imgPath: "public/smileschool HTML&CSS UI.png",
+  },
+  {
+    projectTitle: "Expense Tracker",
+    category: "react",
+    imgPath: "public/expense tracker react.png",
+  },
+  {
+    projectTitle: "Dynamic to do list",
+    category: "javascript",
+    imgPath: "public/dynamic todo list.png",
+  },
 ];
 
 const MainContent = () => {
   const [selected, setSelected] = useState("all");
   const [arr, setarr] = useState(myprojects);
+
+
+//create a fun that can take btncategory and filter the array based on this category
+const handleClick = (categoryButton) => {
+  setSelected(categoryButton);
+
+  const filteredProjects = myprojects.filter(
+    (project) => project.category === categoryButton
+  );
+  setarr(filteredProjects)
+}
+
+
 
   return (
     <main className="flex gap-5 items-start  ">
@@ -21,64 +59,55 @@ const MainContent = () => {
             setSelected("all");
             setarr(myprojects);
           }}
-          className={selected === "all" ? "active " : "buttons"}
+          className={selected === "all" ? "active " : "buttons"} //in case of active drop .active styles if not keep .buttons styles
         >
           All Projects
         </button>
 
         <button
           onClick={() => {
-            setSelected("htmlcss");
-
-            const filteredProjects = myprojects.filter(
-              (project) => project.category === "htmlcss"
-            ); // Filter for HTML & CSS
-            setarr(filteredProjects); // Set filtered projects array
+            handleClick("htmlcss")// Filter for HTML & CSS
           }}
-          className={selected === "htmlcss" ? "active " : "buttons"}
+          className={selected === "htmlcss" ? "active " : "buttons"} //in case of active drop .active styles if not keep .buttons style
         >
           HTML & CSS
         </button>
         <button
           onClick={() => {
-            setSelected("javascript");
+          handleClick("javascript")
           }}
-          className={selected === "javascript" ? "active " : "buttons"}
+          className={selected === "javascript" ? "active " : "buttons"} //in case of active drop .active styles if not keep .buttons style
         >
           java script
         </button>
         <button
           onClick={() => {
-            setSelected("react");
+          handleClick("react")
           }}
-          className={selected === "react" ? "active " : "buttons"}
+          className={selected === "react" ? "active " : "buttons"} //in case of active drop .active styles if not keep .buttons style
         >
           React.js
         </button>
         <button
           onClick={() => {
-            setSelected("figma");
+            handleClick("figma")
           }}
-          className={selected === "figma" ? "active " : "buttons"}
+          className={selected === "figma" ? "active " : "buttons"} //in case of active drop .active styles if not keep .buttons style
         >
           UI FIGMA
         </button>
       </section>
 
       <section className="right-section flex flex-wrap justify-center ">
-        {arr.map((item, index) => {
+        {arr.map((project, index) => {
           return (
             <article
               key={index}
               className="card border w-72 rounded-xl hover:border-blue-400 cursor-pointer m-8"
             >
-              <img
-                src={item.imgPath}
-                alt=""
-                className="rounded-xl"
-              />
+              <img src={project.imgPath} alt="" className="rounded-xl" />
               <div className="box py-4 px-2">
-                <h1 className="text-white font-bold">{item.projectTitle}</h1>
+                <h1 className="text-white font-bold">{project.projectTitle}</h1>
                 <p className="text-white text-sm py-4">
                   Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                   Amet, quas ut beatae fuga repudiandae adipisci, labore

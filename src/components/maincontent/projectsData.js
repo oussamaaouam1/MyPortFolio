33da982669146adbb4a704d641436c7b5249f637
oussamaaouam1 <oussama.aouam@gmail.com>
2024-11-20 20:24:00 +0100

    changing imges path 2

--- src/components/maincontent/projectsData.js
+++ src/components/maincontent/projectsData.js
@@ -2,32 +2,32 @@ const myprojects = [
   {
     projectTitle: "Currency Converter",
     category: "react",
-    imgPath: "../../../public/currency converter react.png",
+    imgPath: "/currency converter react.png",
   },
   {
     projectTitle: "Headphones",
     category: "htmlcss",
-    imgPath: "../../../public/headphones HTML&CSS.png",
+    imgPath: "/headphones HTML&CSS.png",
   },
   {
     projectTitle: "Landing Page",
     category: "react",
-    imgPath: "../../../public/landing page react.png",
+    imgPath: "/landing page react.png",
   },
   {
     projectTitle: "html&css project",
     category: "htmlcss",
-    imgPath: "../../../public/smileschool HTML&CSS UI.png",
+    imgPath: "/smileschool HTML&CSS UI.png",
   },
   {
     projectTitle: "Expense Tracker",
     category: "react",
-    imgPath: "../../../public/expense tracker react.png",
+    imgPath: "/expense tracker react.png",
   },
   {
     projectTitle: "Dynamic to do list",
     category: "javascript",
-    imgPath: "../../../public/dynamic todo list.png",
+    imgPath: "/dynamic todo list.png",
   },
 ];
 
